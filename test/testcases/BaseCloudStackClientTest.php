2055a939ebe80c4c6bdbcee60bec28989deb444c
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-14 04:41:34 -0700

    added signature tests

--- test/testcases/BaseCloudStackClientTest.php
+++ test/testcases/BaseCloudStackClientTest.php
@@ -3,10 +3,23 @@ require_once dirname(__FILE__) . "/../../src/BaseCloudStackClient.php";
 require_once 'PHPUnit/Framework/TestCase.php';
 
 Class BaseCloudStackClientTest extends PHPUnit_Framework_TestCase {
+    /***************
+      Helpers
+     ***************/
     public function setExpectedExceptionCode($code) {
         $this->setExpectedException('CloudStackClientException', "", $code);
     }
     
+    protected static function getMethod($name) {
+        $class = new ReflectionClass('MyClass');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
+    
+    /***************
+      Tests
+     ***************/
     public function test_endpoint_empty() {
         $this->setExpectedExceptionCode(ENDPOINT_EMPTY);
         new BaseCloudStackClient("", "-", "-");
@@ -44,6 +57,22 @@ Class BaseCloudStackClientTest extends PHPUnit_Framework_TestCase {
         $this->assertNotEmpty($cloudstack);
         $this->assertEquals($config['api_key'], $cloudstack->apiKey);
         $this->assertEquals($config['secret_key'], $cloudstack->secretKey);
+    }
+    
+    public function test_signature_empty_str() {
+        $this->setExpectedExceptionCode(STRTOSIGN_EMPTY);
+        $cloudstack = new BaseCloudStackClient("http://foo", "-", "-");
+        $cloudstack->getSignature("");
+    }
+    
+    public function test_signature() {
+        // apiKey and secretKey from example code from official API reference
+        // @see http://download.cloud.com/releases/2.2.0/api_2.2.4/user/2.2api_examplecode_details.html
+        $apiKey = "miVr6X7u6bN_sdahOBpjNejPgEsT35eXq-jB8CG20YI3yaxXcgpyuaIRmFI_EJTVwZ0nUkkJbPmY3y2bciKwFQ";
+        $secretKey = "AdpTvsQMZwv9MNvfqDcQhWSL9pHQvq7z13CFP2SU1iToDkkM3d8iZciWEKsGREhMtV9yLVMxmbbJJgrXo8O5jg";
+        $queryString = "apikey=" . strtolower($apiKey) . "&command=listvirtualmachines&response=json";
+        $cloudstack = new BaseCloudStackClient("http://foo", $apiKey, $secretKey);
+        $this->assertEquals($cloudstack->getSignature($queryString), "dPQ94TY7lQAR6tjOVt9smq3xaSY%3D");
     }
 }
 
