0729cdf932afc49709c494c9aebd307240e3f511
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-16 00:43:08 -0700

    added require statement for exceptions

--- src/CloudStackClient.php
+++ src/CloudStackClient.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__FILE__) . "/BaseCloudStackClient.php";
+require_once dirname(__FILE__) . "/CloudStackClientException.php";
 
 class CloudStackClient extends BaseCloudStackClient {
     
