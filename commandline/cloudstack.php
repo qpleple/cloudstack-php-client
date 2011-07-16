8774b74f2961e3a7373ef95b6dbb2c7ec698599f
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-15 22:11:48 -0700

    added initial checks and error handling in command line tool

--- commandline/cloudstack.php
+++ commandline/cloudstack.php
@@ -1,18 +1,50 @@
 <?php
-if (!defined('STDIN')) {
-    echo "This script is supposed to be run from command line. Exiting.\n";
-    exit;
+
+/**************************
+  File paths
+ **************************/
+define("CLOUDSTACKCLIENT_FILE", dirname(__FILE__) . "/../src/CloudStackClient.php");
+define("CONFIG_FILE", dirname(__FILE__) . "/../config.php");
+
+/**************************
+  Initial checks
+ **************************/
+check(defined('STDIN'), "This script is supposed to be run from command line. Exiting.");
+check(file_exists(CLOUDSTACKCLIENT_FILE), "Unable to find CloudStackClient file, looked for " . CLOUDSTACKCLIENT_FILE);
+check(file_exists(CONFIG_FILE), "Unable to find config file, looked for " . CONFIG_FILE);
+check($argc > 1, "Usage: php cloudstack.php command args...");
+
+require CLOUDSTACKCLIENT_FILE;
+$config = require CONFIG_FILE;
+$command = $argv[1];
+$args = array_slice($argv, 1);
+
+/**************************
+  Request
+ **************************/
+$cloudstack = new BaseCloudStackClient($config['endpoint'], $config['api_key'], $config['secret_key']);
+try {
+    $result = $cloudstack->request($command, $args);
+} catch (Exception $e) {
+    err($e->getMessage());
 }
 
-require_once dirname(__FILE__) . "/../src/CloudStackClient.php";
+//check(property_exists($result, strtolower($command)), sprintf("Incorrect command. Received %s", var_dump_str($result)));
 
-try {
-    throw new Exception("haha", 2);
-} catch (Exception $e) {
-    if ($e->getCode() == 0) {
-        printf("Fatal Error: %s\n", $e->getMessage());
-    } else {
-        printf("Fatal Error (code %s): %s\n", $e->getCode(), $e->getMessage());
+/**************************
+  Helpers
+ **************************/
+function check($cond, $errorMessage) {
+    if (!$cond) {
+        err($errorMessage);
     }
-    
+}
+function err($errorMessage) {
+    die("Fatal Error: $errorMessage\n");
+}
+
+function var_dump_str($obj) {
+    ob_start();
+    var_dump($obj);
+    return ob_get_clean();
 }
