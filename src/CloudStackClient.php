9d11fb0f2bcaa42545645349f616668752950d9a
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-15 21:24:38 -0700

    typo

--- src/CloudStackClient.php
+++ src/CloudStackClient.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . "/CloudStackClient.php";
+require_once dirname(__FILE__) . "/BaseCloudStackClient.php";
 
 class CloudStackClient extends BaseCloudStackClient {
     /**
