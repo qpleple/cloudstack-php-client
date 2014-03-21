ac35e5124423434d8e8a72088ef0f1671935d991
Aaron Hurt <ahurt@ena.com>
2014-03-21 17:19:16 -0500

    general cleanup and more updates for 4.3

--- src/BaseCloudStackClient.php
+++ src/BaseCloudStackClient.php
@@ -8,48 +8,47 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 require_once dirname(__FILE__) . "/CloudStackClientException.php";
 
 class BaseCloudStackClient {
     public $apiKey;
     public $secretKey;
     public $endpoint; // Does not ends with a "/"
-	
-	public function __construct($endpoint, $apiKey, $secretKey) {
-	    // API endpoint
-	    if (empty($endpoint)) {
-	        throw new CloudStackClientException(ENDPOINT_EMPTY_MSG, ENDPOINT_EMPTY);
-	    }
-	    
-	    if (!preg_match("|^http://.*$|", $endpoint)) {
-	        throw new CloudStackClientException(sprintf(ENDPOINT_NOT_URL_MSG, $endpoint), ENDPOINT_NOT_URL);
-	    }
-	    
-	    // $endpoint does not ends with a "/"
-	    $this->endpoint = substr($endpoint, -1) == "/" ? substr($endpoint, 0, -1) : $endpoint;
-	    
-	    // API key
-	    if (empty($apiKey)) {
-	        throw new CloudStackClientException(APIKEY_EMPTY_MSG, APIKEY_EMPTY);
-	    }
-		$this->apiKey = $apiKey;
-		
-		// API secret
-		if (empty($secretKey)) {
-		    throw new CloudStackClientException(SECRETKEY_EMPTY_MSG, SECRETKEY_EMPTY);
-		}
-		$this->secretKey = $secretKey;
-	}
-	
+
+    public function __construct($endpoint, $apiKey, $secretKey) {
+        // API endpoint
+        if (empty($endpoint)) {
+            throw new CloudStackClientException(ENDPOINT_EMPTY_MSG, ENDPOINT_EMPTY);
+        }
+
+        if (!preg_match("|^http://.*$|", $endpoint)) {
+            throw new CloudStackClientException(sprintf(ENDPOINT_NOT_URL_MSG, $endpoint), ENDPOINT_NOT_URL);
+        }
+
+        // $endpoint does not ends with a "/"
+        $this->endpoint = substr($endpoint, -1) == "/" ? substr($endpoint, 0, -1) : $endpoint;
+
+        // API key
+        if (empty($apiKey)) {
+            throw new CloudStackClientException(APIKEY_EMPTY_MSG, APIKEY_EMPTY);
+        }
+        $this->apiKey = $apiKey;
+
+        // API secret
+        if (empty($secretKey)) {
+            throw new CloudStackClientException(SECRETKEY_EMPTY_MSG, SECRETKEY_EMPTY);
+        }
+        $this->secretKey = $secretKey;
+    }
+
     public function getSignature($queryString) {
         if (empty($queryString)) {
             throw new CloudStackClientException(STRTOSIGN_EMPTY_MSG, STRTOSIGN_EMPTY);
         }
-        
+
         $hash = @hash_hmac("SHA1", $queryString, $this->secretKey, true);
-        $base64encoded = base64_encode($hash);
-        return urlencode($base64encoded);
+        return urlencode(base64_encode($hash));
     }
 
     public function request($command, $args = array()) {
@@ -66,47 +65,63 @@ class BaseCloudStackClient {
                 unset($args[$key]);
             }
         }
-        
+
         // Building the query
         $args['apikey'] = $this->apiKey;
         $args['command'] = $command;
         $args['response'] = "json";
         ksort($args);
-        $query = http_build_query($args);
-        $query = str_replace("+", "%20", $query);
-        $query .= "&signature=" . $this->getSignature(strtolower($query));
-    
-        $httpRequest = new HttpRequest();
-        $httpRequest->setMethod(HTTP_METH_POST);
-        $url = $this->endpoint . "?" . $query;
+        $query = str_replace("+", "%20", http_build_query($args));
+        $query = sprintf("%s&signature=%s", $query, $this->getSignature(strtolower($query)));
 
-        $httpRequest->setUrl($url);
-    
-        $httpRequest->send();
-        
-        $code =$httpRequest->getResponseCode();
-        $data = $httpRequest->getResponseData();
+        // Initialize curl
+        $ch = curl_init();
+        $curl_opts = array(
+            CURLOPT_URL => $this->endpoint,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $query,
+            CURLOPT_RETURNTRANSFER => true,
+        );
+        curl_setopt_array($ch, $curl_opts);
+
+        // Execute curl to get data and return code
+        $data = curl_exec($ch);
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        // Close curl - free handle
+        curl_close($ch);
+
+        //echo "data == $data\n";
         if (empty($data)) {
             throw new CloudStackClientException(NO_DATA_RECEIVED_MSG, NO_DATA_RECEIVED);
         }
-        //echo $data['body'] . "\n";
-        $result = @json_decode($data['body']);
+
+        $result = @json_decode($data);
         if (empty($result)) {
             throw new CloudStackClientException(NO_VALID_JSON_RECEIVED_MSG, NO_VALID_JSON_RECEIVED);
         }
-        
-        $propertyResponse = strtolower($command) . "response";
-        
+
+        $propertyResponse = sprintf("%sresponse", strtolower($command));
+
+        // standard presentation of errors
+        if (property_exists($result, "errorresponse") && property_exists($result->errorresponse, "errortext")) {
+            throw new CloudStackClientException($result->errorresponse->errortext);
+        }
+
         if (!property_exists($result, $propertyResponse)) {
-            if (property_exists($result, "errorresponse") && property_exists($result->errorresponse, "errortext")) {
-                throw new CloudStackClientException($result->errorresponse->errortext);
-            } else {
-                throw new CloudStackClientException(sprintf("Unable to parse the response. Got code %d and message: %s", $code, $data['body']));
+            // some commands drop the trailing 's' in the response: listPools becomes 'listpoolresponse'
+            $propertyResponse = sprintf("%sresponse", substr(strtolower($command), 0, -1));
+            if (!property_exists($result, $propertyResponse)) {
+                throw new CloudStackClientException(sprintf("Unable to parse the response. Got code %d and message: %s", $code, $data));
             }
         }
-        
+
         $response = $result->{$propertyResponse};
-        
+
+        // sometimes we get errorcode and errortext inside the command response
+        if (property_exists($response, "errorcode") && property_exists($response, "errortext")) {
+            throw new CloudStackClientException($response->errortext);
+        }
+
         // list handling : most of lists are on the same pattern as listVirtualMachines :
         // { "listvirtualmachinesresponse" : { "virtualmachine" : [ ... ] } }
         preg_match('/list(\w+)s/', strtolower($command), $listMatches);
@@ -120,7 +135,7 @@ class BaseCloudStackClient {
             } else {
                 // sometimes, the 's' is kept, as in :
                 // { "listasyncjobsresponse" : { "asyncjobs" : [ ... ] } }
-                $objectName = $listMatches[1] . "s";
+                $objectName = sprintf("%ss", $listMatches[1]);
                 if (property_exists($response, $objectName)) {
                     $resultArray = $response->{$objectName};
                     if (is_array($resultArray)) {
@@ -129,7 +144,7 @@ class BaseCloudStackClient {
                 }
             }
         }
-        
+
         return $response;
     }
 }
